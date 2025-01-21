f13f603a3c1e4bb89d9984f32dbebf9474e08b18
Sander <sanderlandm@gmail.com>
2025-01-21 12:11:54 +0100

    ADD The api now only works if the client specifies it accepts json

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import evidence from 'routers/evidence.js';
+import evidence from './routes/evidence.js';
+import cases from './routes/cases.js';
+import profiles from './routes/profiles.js';
+import games from './routes/games.js';
 
 const app = express();
 
@@ -12,6 +15,39 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Global middleware
+//Make sure the client is informed this webservice only sends JSON
+app.use((req, res, next) => {
+
+    //If the client did not specify it accepts JSON, send an error unless they only ask for options
+    if (!req.headers.accept?.includes('application/json') && req.method !== 'OPTIONS') {
+
+        res.status(406);
+        return res.json({error: 'This webservice only responds with JSON. Please specify if you will accept this format.'})
+
+    }
+
+    //Otherwise just continue
+    next();
+
+});
+
+//Add CORS headers to all responses
+app.use((req, res, next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
+
+    next();
+
+});
 
 //Use imported routes
 app.use('/evidence', evidence);
+app.use('/cases', cases);
+app.use('/profiles', profiles);
+app.use('/games', games);
+
+//Print the port to the console so we know when it's actually running
+app.listen(process.env.EXPRESS_PORT, () => {
+    console.log(`Server is now listening on port ${process.env.EXPRESS_PORT}`);
+});
