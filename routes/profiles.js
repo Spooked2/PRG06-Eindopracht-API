0d1114923dae5c16fb7efd2f185e78ce120dad91
Sander <sanderlandm@gmail.com>
2025-01-24 15:51:18 +0100

    ADD profiles can now be updated and their detail shown

--- routes/profiles.js
+++ routes/profiles.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 
     try {
 
-        const profiles = await Profile.find({}).populate("case", "name").exec();
+        const profiles = await Profile.find({}).populate("cases", "name").exec();
 
         res.status(200);
         res.json(profiles);
@@ -129,6 +129,110 @@ router.use('/:id', async (req, res, next) => {
     } catch (error) {
 
         res.status(500);
+        res.json({error: error.message});
+
+    }
+
+});
+
+//Get the details of a specific profile
+router.get('/:id', async (req, res) => {
+
+    try {
+
+        const profile = await Profile.findById(req.params.id)
+            .populate("cases", "name")
+            .populate("descriptions.description.case", "name");
+
+
+        res.status(200);
+        res.json(profile);
+
+    } catch (error) {
+
+        res.status(500);
+        res.json({error: error.message});
+
+    }
+
+});
+
+//Update a specific profile
+router.put('/:id', async (req, res) => {
+
+    //Collect all the case ids in the request in a set to prevent duplicates
+    let allCaseIds = new Set();
+
+    for (const caseId of req.body.cases) {
+        allCaseIds.add(caseId);
+    }
+
+    for (const description of req.body.descriptions) {
+        allCaseIds.add(description.case);
+    }
+
+    for (const caseId of allCaseIds) {
+
+        //Check if the given case ids are valid
+        if (!mongoose.Types.ObjectId.isValid(caseId)) {
+
+            res.status(400);
+            return res.json({error: 'Given case id is invalid', invalidId: caseId});
+
+        }
+
+        //Make sure the given case actually exists
+        if (!await Case.exists({_id: caseId})) {
+
+            res.status(404);
+            return res.json({error: 'Case could not be found', requestedCase: caseId});
+
+        }
+
+    }
+
+    try {
+
+        await Profile.validate(req.body);
+
+        const oldProfile = await Profile.findById(req.params.id);
+        const newCaseIds = new Set(req.body.cases);
+        const oldCaseIds = new Set(oldProfile.cases);
+
+        const postedProfile = req.body;
+        Object.assign(oldProfile, postedProfile);
+
+        const updatedProfile = await oldProfile.save();
+
+        const addedCases = [...newCaseIds].filter(caseId => !oldCaseIds.has(caseId));
+        const removedCases = [...oldCaseIds].filter(caseId => !newCaseIds.has(caseId));
+
+        if (addedCases.length > 0) {
+
+            for (const addedCase of addedCases) {
+
+                await Case.findByIdAndUpdate(addedCase, {$addToSet: {profiles: updatedProfile._id}});
+
+            }
+
+        }
+
+        if (removedCases.length > 0) {
+
+            for (const removedCase of removedCases) {
+
+                await Case.findByIdAndUpdate(removedCase, {$pull: {profiles: updatedProfile._id}});
+
+            }
+
+        }
+
+        res.status(200);
+        res.json({success: true, profile: updatedProfile});
+
+    } catch (error) {
+
+        res.status(error instanceof mongoose.Error.ValidationError ? 400 : 500);
         res.json({error: error.message});
 
     }
