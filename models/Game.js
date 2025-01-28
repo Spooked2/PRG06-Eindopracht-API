4fd3a4356c42cd1ddf16829a5e307862c56eb802
Sander <sanderlandm@gmail.com>
2025-01-28 14:53:16 +0100

    CHANGE Game model now wants the release_year as a string so the checker can work

--- models/Game.js
+++ models/Game.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Game = new mongoose.Schema({
     full_name: {type: String, required: true, unique: true},
     short_name: {type: String, required: true, unique: true},
-    release_year: {type: Number, required: true},
+    release_year: {type: String, required: true},
     cases: [{type: mongoose.Types.ObjectId, required: true, ref: "Case"}]
 });
 
